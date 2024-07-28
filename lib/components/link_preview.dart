5c8dc9e6e4c5774698eeeb32d71a520475764fb2
Thanakorn Thanom <77257740+ThanakornThanom@users.noreply.github.com>
2024-07-28 22:20:19 +0700

    fix ui blink

--- lib/components/link_preview.dart
+++ lib/components/link_preview.dart
@@ -3,10 +3,45 @@ import 'package:any_link_preview/any_link_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-class CustomLinkPreview extends StatelessWidget {
+class CustomLinkPreview extends StatefulWidget {
   final String url;
 
   const CustomLinkPreview({super.key, required this.url});
+
+  @override
+  _CustomLinkPreviewState createState() => _CustomLinkPreviewState();
+}
+
+class _CustomLinkPreviewState extends State<CustomLinkPreview> {
+  Metadata? _metadata;
+  bool _isLoading = true;
+  late String _displayUrl;
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchMetadata();
+    _displayUrl = _getDisplayUrl(widget.url);
+  }
+
+  Future<void> _fetchMetadata() async {
+    final metadata = await AnyLinkPreview.getMetadata(link: widget.url);
+    if (mounted) {
+      setState(() {
+        _metadata = metadata;
+        _isLoading = false;
+      });
+    }
+  }
+
+  String _getDisplayUrl(String url) {
+    final uri = Uri.parse(url);
+    String host = uri.host;
+    if (host.startsWith('www.')) {
+      host = host.substring(4);
+    }
+    return host;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,76 +53,67 @@ class CustomLinkPreview extends StatelessWidget {
     );
 
     return Container(
-      decoration: BoxDecoration(
-        border: Border.all(
-          color:
-              Provider.of<AmityUIConfiguration>(context).appColors.baseShade4,
-        ),
-        borderRadius: const BorderRadius.all(
-          Radius.circular(8),
-        ),
-      ),
-      child: FutureBuilder<Metadata?>(
-        future: AnyLinkPreview.getMetadata(link: url),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            if (snapshot.hasData) {
-              final data = snapshot.data!;
-              return Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  if (data.image != null)
-                    Image.network(
-                      data.image!,
-                      width: double.infinity,
-                      height: 200,
-                      fit: BoxFit.cover,
+        child: _isLoading
+            ? const SizedBox()
+            : _metadata != null
+                ? Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Provider.of<AmityUIConfiguration>(context)
+                            .appColors
+                            .baseShade4,
+                      ),
+                      borderRadius: const BorderRadius.all(
+                        Radius.circular(8),
+                      ),
                     ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 8.0, top: 8.0),
-                    child: Text(
-                      url,
-                      style: style.copyWith(
-                          fontSize: 14,
-                          color: Colors.grey[600],
-                          fontWeight: FontWeight.w400),
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                  ),
-                  Padding(
-                    padding:
-                        const EdgeInsets.only(left: 8.0, bottom: 16.0, top: 8),
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        if (data.title != null)
-                          Text(
-                            data.title!,
-                            style: style,
-                            maxLines: 2,
+                        if (_metadata!.image != null)
+                          Image.network(
+                            _metadata!.image!,
+                            width: double.infinity,
+                            height: 200,
+                            fit: BoxFit.cover,
+                          ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 8.0, top: 8.0),
+                          child: Text(
+                            _displayUrl,
+                            style: style.copyWith(
+                                fontSize: 14,
+                                color: Colors.grey[600],
+                                fontWeight: FontWeight.w400),
                             overflow: TextOverflow.ellipsis,
                           ),
-                        if (data.desc != null)
-                          Text(
-                            data.desc!,
-                            style: style.copyWith(fontSize: 14),
-                            maxLines: 3,
-                            overflow: TextOverflow.ellipsis,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(
+                              left: 8.0, bottom: 16.0, top: 8),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              if (_metadata!.title != null)
+                                Text(
+                                  _metadata!.title!,
+                                  style: style,
+                                  maxLines: 2,
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                              if (_metadata!.desc != null)
+                                Text(
+                                  _metadata!.desc!,
+                                  style: style.copyWith(fontSize: 14),
+                                  maxLines: 3,
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                            ],
                           ),
+                        ),
                       ],
                     ),
-                  ),
-                ],
-              );
-            } else {
-              return const Center(
-                  child: Text('Failed to load preview', style: style));
-            }
-          } else {
-            return const Center(child: CircularProgressIndicator());
-          }
-        },
-      ),
-    );
+                  )
+                : const SizedBox());
   }
 }

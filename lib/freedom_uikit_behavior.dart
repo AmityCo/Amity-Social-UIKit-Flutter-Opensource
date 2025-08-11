e12d571e842822d6a5c47c8c2044f546d9b7d72c
arman-garousi-dev <arman.garousi.dev@gmail.com>
2025-08-11 10:41:17 +0330

    feat(BFDW-13708): add Amity config behavior

--- lib/freedom_uikit_behavior.dart
+++ lib/freedom_uikit_behavior.dart
@@ -1,3 +1,4 @@
+import 'package:amity_uikit_beta_service/v4/core/amity_config_behavior.dart';
 import 'package:amity_uikit_beta_service/v4/social/social_home_page/amity_social_home_page_behavior.dart';
 
 class FreedomUIKitBehavior {
@@ -7,6 +8,9 @@ class FreedomUIKitBehavior {
   static final FreedomUIKitBehavior _instance = FreedomUIKitBehavior._internal();
   FreedomUIKitBehavior._internal();
 
+  AmityConfigBehavior configBehavior =
+      AmityConfigBehavior();
+  
   AmitySocialHomePageBehavior socialHomePageBehavior =
       AmitySocialHomePageBehavior();
 }

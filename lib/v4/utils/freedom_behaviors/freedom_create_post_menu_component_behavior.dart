1ae8832f1a1875134894fe773504016c309418f4
arman-garousi-dev <arman.garousi.dev@gmail.com>
2025-08-30 02:07:55 +0330

    feat(BFDW-14060): add getStoryTargets behavior

--- lib/v4/utils/freedom_behaviors/freedom_create_post_menu_component_behavior.dart
+++ lib/v4/utils/freedom_behaviors/freedom_create_post_menu_component_behavior.dart
@@ -1,9 +1,18 @@
+import 'package:amity_sdk/amity_sdk.dart';
 import 'package:amity_uikit_beta_service/v4/core/toast/amity_uikit_toast.dart';
 import 'package:amity_uikit_beta_service/v4/core/toast/bloc/amity_uikit_toast_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 class FreedomCreatePostMenuComponentBehavior {
+  final int storyTargetsPageSize = 50;
+
+  Future<List<AmityCommunity>> getStoryTargets({
+    required List<AmityCommunity> communities,
+  }) async {
+    return communities;
+  }
+
   void showStorySuccessToast(BuildContext context) {
     context.read<AmityToastBloc>().add(
           const AmityToastShort(

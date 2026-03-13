dbf32d9df10aec723113ac959dd5f608f7b42611
Mat Grzegorzek <mateusz.grzegorzek@rivia.net>
2026-03-13 09:35:23 +0100

    FIx My Groups UI

--- lib/v4/social/my_community/my_community_component.dart
+++ lib/v4/social/my_community/my_community_component.dart
@@ -30,21 +30,13 @@ class AmityMyCommunitiesComponent extends NewBaseComponent {
             if (state is MyCommunityLoading) {
               return communitySkeletonList(theme, configProvider);
             } else if (state is MyCommunityLoaded) {
-              return Expanded(
-                child: Column(
-                  children: [
-                    // Container(
-                    //   color: theme.baseColorShade4,
-                    //   height: 8,
-                    // ),
-                    Expanded(
-                      child: communityList(context, scrollController, state.list, theme, () {
-                        context.read<MyCommunityBloc>().add(MyCommunityEventLoadMore());
-                      }),
-                    ),
-                  ],
+              return Column(children: [
+                Expanded(
+                  child: communityList(context, scrollController, state.list, theme, () {
+                    context.read<MyCommunityBloc>().add(MyCommunityEventLoadMore());
+                  }),
                 ),
-              );
+              ]);
             } else {
               return Container();
             }
